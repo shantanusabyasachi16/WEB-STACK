e5b7f2f19f8a53f4dd41a0da3a87b6b809cc37a9
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-06-26 09:05:29 +0530

    managing the state , components and props

--- react01/src/App.jsx
+++ react01/src/App.jsx
@@ -1,13 +1,48 @@
+  
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+ 
 function App() {
+  //state
+  const [todo, settodo] = useState({
+    Title : "go to gym",
+  Descriptions: "go to gym at 7pm",
+  id : 1
+  })
+  
+  setInterval(()=>{
+settodo({
+  Title : "go to gymmm",
+  Descriptions: "go to play at 7am",
+  id : 2
+
+  })
+},2000)
+ 
  
   return (
-    <div>hiiiiiiii</div>
-  )
-} 
+    <div>
+      {todo.Title}
+       <br />
+      {todo.Descriptions}
+      <br />
+      {todo.id}
+    <Name firstname={todo.Title}></Name>
 
+    </div>
+  )
+}  
+
+function Name (props){
+  return <div>
+    <br />
+    {props.firstname}
+    <br />
+    {props.lastname}
+  </div>
+}
+  
 export default App
+
+//we can  change the state from one components to the child components and  the child components can respond to it
+ 
