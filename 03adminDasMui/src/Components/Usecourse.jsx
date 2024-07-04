fbff2c59b2e175a5f2ffdd47092480f57489a03f
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-07-04 14:25:04 +0530

    debugging

--- 03adminDasMui/src/Components/Usecourse.jsx
+++ 03adminDasMui/src/Components/Usecourse.jsx
@@ -1,142 +1,126 @@
-import { Card ,Typography, TextField, Button} from '@mui/material';
-import  { useEffect,useState} from 'react'
+import { Card, Typography, TextField, Button } from '@mui/material';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-function Usecourse() { 
- 
-  let { courseId } = useParams();
+function Usecourse() {
+  let { courseId } = useParams();//to extraxt the id of th courses
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    const[courses,setcourses]= useState([]);
-     useEffect(()=>{
-   fetch("http://localhost:3000/admin/courses",{
-    method:"GET",
-    headers: {
-      
-      "Authorization" :"Bearer " + localStorage.getItem("token")
-  
-    }
-   }).then((res)=>{
-return res.json()
-   }).then((data)=>{
-  setcourses(data.courses)
-   })
-     },[])
-
-let course;
-for (let i = 0; i < courses.length; i++) {
-  if(courses[i].id==courseId)[
-    course=courses[i] 
-  ]
-  
-}
-  
-if(!course){
-  return<div>loading....</div>
-}    
- 
- return(
-  <Coursecard course={course}/>,
-  <Updatecard course={course}/>
- ) 
-
-  function Updatecard(props){
-
-    const [Title,setTitle] = useState("");
-    const [Description,setDescription] = useState("");
-    const [Image,setImage] = useState("");
-
-const course =props.course;
-
-    <div style={{display:"flex",justifyContent:"center"}}>
-      
-  <Card variant="outlined" style={{width: 400 , padding:20}}>
-    <Typography>Upadate course details</Typography>
-      <TextField  fullWidth
-      onChange={(e)=>{
-        setTitle(e.target.value)
-         
-      }}
-
-      label="Title" variant="outlined" />
-        <br/><br/>
-
-
-      <TextField  fullWidth
-     onChange={(e)=>{
-   setDescription(e.target.value)
-    }}
-        label="Description" variant="outlined" />
-
-<br /><br/>
-
-      <TextField  fullWidth
-     onChange={(e)=>{
-   setImage(e.target.value)
-    }}
-        label="Imagelink" variant="outlined" />
-
-
-     <br /><br/>
-
-
-       <Button variant="contained"
-       onClick={()=>{
-    
-     fetch("http://localhost:3000/admin/courses/"+course.id,{
-      method:"PUT",
-      body: JSON.stringify({
-       title:Title,
-       description:Description,
-       imageLink:Image,
-       published:true
-      }),
+  useEffect(() => {
+    fetch("http://localhost:3000/admin/courses", {
+      method: "GET",
       headers: {
-        "content-type" : "application/json",
-        "Authorization" :"Bearer " + localStorage.getItem ("token")
-
+        "Authorization": "Bearer " + localStorage.getItem("token")
       }
-     }).then((res)=>{
-      return res.json() 
+    }).then((res) => {
+      return res.json();
+    }).then((data) => {
+      setCourses(data.courses);
+      setLoading(false);
+    }).catch((error) => {
+      console.error("Error fetching courses:", error);
+      setLoading(false);
+    });
+  }, []);
 
-     }).then((data)=>{
-    alert("added courses");
-     })
-
-       }}
-       
-       >Updatecousrse</Button>
-     
-       </Card>
-       
- </div>
+  if (loading) {
+    return <div>Loading....</div>;
   }
 
+  const course = courses.find(c => c.id.toString() === courseId);
 
+  if (!course) {
+    return <div>Course not found</div>;
+  }
 
-    function Coursecard(props) {
-      const course= props.course;
- return(
-  <div>
-  <Card style={{
-  margin:10,
-  width:300,
-  minHeight:200
-}}>
-    <Typography textAlign={"center"} variant='h6'> {course.title}</Typography>
-    <Typography textAlign={"center"} variant='h6'>{course.description}</Typography>
-    <img src={course.imageLink} style={{width:300}}></img>
-    
-    </Card>
+  return (
+    <div>
+      <Coursecard course={course} />
+      <Updatecard course={course} />
     </div>
-      )
-    }
-  
-   
- } 
-export default Usecourse
+  );
 
+  function Updatecard(props) {
+    const [Title, setTitle] = useState("");
+    const [Description, setDescription] = useState("");
+    const [Image, setImage] = useState("");
 
+    const course = props.course;
 
+    return (
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Card variant="outlined" style={{ width: 400, padding: 20 }}>
+          <Typography>Update course details</Typography>
+          <TextField
+            fullWidth
+            onChange={(e) => setTitle(e.target.value)}
+            label="Title"
+            variant="outlined"
+          />
+          <br /><br />
+          <TextField
+            fullWidth
+            onChange={(e) => setDescription(e.target.value)}
+            label="Description"
+            variant="outlined"
+          />
+          <br /><br />
+          <TextField
+            fullWidth
+            onChange={(e) => setImage(e.target.value)}
+            label="Image link"
+            variant="outlined"
+          />
+          <br /><br />
+          <Button
+            variant="contained"
+            onClick={() => {
+              fetch("http://localhost:3000/admin/courses/" + course.id, {
+                method: "PUT",
+                body: JSON.stringify({
+                  title: Title,
+                  description: Description,
+                  imageLink: Image,
+                  published: true
+                }),
+                headers: {
+                  "Content-Type": "application/json",
+                  "Authorization": "Bearer " + localStorage.getItem("token")
+                }
+              }).then((res) => {
+                return res.json();
+              }).then((data) => {
+                alert("Course updated successfully");
+              }).catch((error) => {
+                console.error("Error updating course:", error);
+                alert("Failed to update course");
+              });
+            }}
+          >
+            Update course
+          </Button>
+        </Card>
+      </div>
+    );
+  }
 
+  function Coursecard(props) {
+    const course = props.course;
+    return (
+      <div style={{display:"flex",justifyContent:"center"}}>
+        <Card style={{
+          margin: 10,
+          width: 300,
+          minHeight: 200
+        }}>
+          <Typography textAlign={"center"} variant='h6'>{course.title}</Typography>
+          <Typography textAlign={"center"} variant='h6'>{course.description}</Typography>
+          <img src={course.imageLink} style={{ width: 300 }} alt="course" />
+        </Card>
+      </div>
+    );
+  }
+}
 
-
+export default Usecourse;
